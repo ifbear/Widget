af979ab787e7dda439ac48c1e207b525ed6283ce
dexiong <dexiong@staff.sina.com>
2023-05-04 16:20:16 +0800

    Initial Commit

---
+++ Widget/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Widget
+//
+//  Created by dexiong on 2023/5/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
